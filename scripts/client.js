b6d3b4f654420fcfe514990bb38d415c86bb3b43
Alex White <alexlloydwhite@gmail.com>
2021-04-09 15:58:04 -0500

    input form & table created

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,25 @@
 $(document).ready(onReady);
-console.log('JS');
+
 function onReady(){
-    console.log('JQ');
+    $('#submit').on('click', handleSubmit);
 }
 
+function handleSubmit(){
+    console.log('Adding Employee'); 
+    let firstName = $('#firstNameIn').val();
+    let lastName = $('#lastNameIn').val();
+    let id = $('#idIn').val();
+    let title = $('#titleIn').val();
+    let annualSalary = $('#annualSalaryIn').val();
+    
+    $('#employeeTable').append(`
+            <tr>
+                <td>${firstName}</td>
+                <td>${lastName}</td>
+                <td>${id}</td>
+                <td>${title}</td>
+                <td>${annualSalary}</td>
+                <td><button class="delete">Delete</button></td>   
+            </tr>
+        `);
+}
