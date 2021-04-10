2fde26d17b92d054bc2e3d3cc989b330f237be8e
Alex White <alexlloydwhite@gmail.com>
2021-04-10 18:48:46 -0500

    styled table, reworked logic to store employee data in an array

--- scripts/client.js
+++ scripts/client.js
@@ -1,44 +1,89 @@
 $(document).ready(onReady);
 
 function onReady(){
-    $('#submit').on('click', handleSubmit);
+    $('#submit').on('click', addEmployee);
     $('#employeeTable').on('click', '.delete', deleteEmployee);
+    showEmployees();
 }
 
-let totalSalary = 0;
-
-function handleSubmit(){
-    addEmployee();
-    clearInputs();
-}
+const employees = [
+    {
+        id: 4521,
+        firstName: 'Jen',
+        lastName: 'Barber',
+        title: 'Team Lead',
+        annualSalary: 80000,
+    },
+    {
+        id: 8724,
+        firstName: 'Maurice',
+        lastName: 'Moss',
+        title: 'Support Team',
+        annualSalary: 58000,
+    },
+    {
+        id: 9623,
+        firstName: 'Roy',
+        lastName: 'Smith',
+        title: 'Quality Assurance', 
+        annualSalary: 48000,
+    },
+];
 
 function addEmployee(){
-    console.log('Adding Employee'); 
-    let firstName = $('#firstNameIn').val();
-    let lastName = $('#lastNameIn').val();
-    let id = $('#idIn').val();
-    let title = $('#titleIn').val();
-    let annualSalary = Number($('#annualSalaryIn').val());
-    
-    totalSalary += annualSalary;
-    let monthlySalary = totalSalary / 12;
-    $('#totalMonthlyOut').empty().append(monthlySalary);
-    if(monthlySalary >= 20000){
-        addRedBackground();
+    console.log('Adding Employee');
+    // collects informaton from add employee inputs as an object
+    let newEmployee ={
+        id: $('#idIn').val(),
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        title: $('#titleIn').val(),
+        annualSalary: $('#annualSalaryIn').val(),
     }
-
-    $('#employeeTable').append(`
-        <tr>
-            <td>${firstName}</td>
-            <td>${lastName}</td>
-            <td>${id}</td>
-            <td>${title}</td>
-            <td>${annualSalary}</td>
-            <td><button class="delete">Delete</button></td>   
-        </tr>
-    `);
+    // adds newEmployee object to employees array
+    employees.push(newEmployee);
+    // clear inputs
+    clearInputs();
+    //display employees on the DOM
+    showEmployees();
 }
 
+function showEmployees(){
+    // empty's table
+    $('#employeeTable').empty();
+
+    // loops thru employees array, appends every object to the DOM
+    for(let employee of employees){
+        $('#employeeTable').append(`
+        <tr>
+            <td>${employee.id}</td>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.title}</td>
+            <td>${employee.annualSalary}</td>
+            <td><button class="delete">Delete</button></td>   
+        </tr>`);
+    }
+
+    calculateSalary();
+}
+
+function calculateSalary(){
+   
+    let totalSalary = 0;
+    
+    for (employee of employees){
+        totalSalary += Number(employee.annualSalary);
+    }
+    
+    $('#totalMonthlyOut').empty().append(Math.round(totalSalary /12 ));
+    
+    if (totalSalary /12 >= 20000){
+        addRedBackground();
+    }
+}
+
+// clears inputs
 function clearInputs(){
     $('#firstNameIn').val('');
     $('#lastNameIn').val('');
